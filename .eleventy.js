5847dcab3a227d5cc62d72b5d437bada864cae46
Naeem Maroof <naeemmaroof@gmail.com>
2025-04-19 19:39:36 -0400

    Add updated site

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function (eleventyConfig) {
+  return {
+    pathPrefix: "/connect4good",
+    dir: {
+      input: ".",
+      output: "docs",
+      includes: "_includes"
+    }
+  };
+};
---
+++ blog/post-1.md
@@ -0,0 +1,8 @@
+---
+layout: base.njk
+title: Post 1
+---
+
+# This is the first blog post
+
+Welcome to the Connect4Good blog!
